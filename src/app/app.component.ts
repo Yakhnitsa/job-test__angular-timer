7e6f5dd9d8a9d4e585989b9f28732b90c1cca3db
Yuriy work-pc <transpmailbox@gmail.com>
2021-01-14 12:03:20 +0200

    format date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,61 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {interval, SubscriptionLike} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-timer';
-  timerValue = '00:00:31'
-  timerActive = true;
+  subscription : SubscriptionLike | undefined;
+  interval: number = 100;
+  timerValue = 0;
+  timerActive = false;
 
   start(){
-    console.log('timer started')
+    this.startAndSubscribe();
+    if(this.timerActive){
+      this.timerActive = false;
+      this.timerValue = 0;
+    }else{
+      this.timerActive = true;
+    }
   };
   wait(){
-    console.log('timer stopped')
+    this.timerActive = false;
   };
   reset(){
-    console.log('timer reset')
+    this.startAndSubscribe();
+    this.timerValue = 0;
+    this.timerActive = true;
+  }
+  private startAndSubscribe(){
+    this.unsubscribe();
+    this.subscription = interval(this.interval)
+      .subscribe( () =>{
+        if(this.timerActive) this.timerValue += this.interval;
+      })
+  }
+  private unsubscribe(){
+    if(this.subscription){
+      this.subscription.unsubscribe();
+      // this.subscription = null;
+    }
+
+  }
+  // private resetIntervalAndSubscribe(){
+  //   // this.interval$ = interval()
+  //   // this.interval$.subscribe( val =>{
+  //   //     if(this.timerActive) this.timerValue++;
+  //   //   })
+  // }
+
+
+  ngOnInit() {
+    // this.interval$.subscribe(val =>{
+    //   if(this.timerActive) this.timerValue++;
+    // })
   }
 
 }
